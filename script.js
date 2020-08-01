5a7cde526dfe5dbf92b125c28127feb7a57980d6
mficco26 <63883987+mficco26@users.noreply.github.com>
2020-08-01 14:50:37 -0600

    fixed more bugs

--- script.js
+++ script.js
@@ -452,16 +452,43 @@ if (queryString == "") {
   document.getElementById("nine").innerHTML = website9;
   document.getElementById("nine").href = href9;
   
-  
+  if (website1 == "") {
+  } else {
   document.getElementById("img1").src =  "http://icons.duckduckgo.com/ip1/" + website1 + ".ico";
-  document.getElementById("img2").src =  "http://icons.duckduckgo.com/ip2/" + website2 + ".ico";
-  document.getElementById("img3").src =  "http://icons.duckduckgo.com/ip3/" + website3 + ".ico";
-  document.getElementById("img4").src =  "http://icons.duckduckgo.com/ip4/" + website4 + ".ico";
-  document.getElementById("img5").src =  "http://icons.duckduckgo.com/ip5/" + website5 + ".ico";
-  document.getElementById("img6").src =  "http://icons.duckduckgo.com/ip6/" + website6 + ".ico";
-  document.getElementById("img7").src =  "http://icons.duckduckgo.com/ip7/" + website7 + ".ico";
-  document.getElementById("img8").src =  "http://icons.duckduckgo.com/ip8/" + website8 + ".ico";
-  document.getElementById("img9").src =  "http://icons.duckduckgo.com/ip9/" + website9 + ".ico";
+  }
+  if (website2 == "") {
+  } else {
+  document.getElementById("img1").src =  "http://icons.duckduckgo.com/ip1/" + website2 + ".ico";
+  }
+  if (website3 == "") {
+  } else {
+  document.getElementById("img1").src =  "http://icons.duckduckgo.com/ip1/" + website3 + ".ico";
+  }
+  if (website4 == "") {
+  } else {
+  document.getElementById("img1").src =  "http://icons.duckduckgo.com/ip1/" + website4 + ".ico";
+  }
+  if (website5 == "") {
+  } else {
+  document.getElementById("img1").src =  "http://icons.duckduckgo.com/ip1/" + website5 + ".ico";
+  }
+  if (website6 == "") {
+  } else {
+  document.getElementById("img1").src =  "http://icons.duckduckgo.com/ip1/" + website6 + ".ico";
+  }
+  if (website7 == "") {
+  } else {
+  document.getElementById("img1").src =  "http://icons.duckduckgo.com/ip1/" + website7 + ".ico";
+  }
+  if (website8 == "") {
+  } else {
+  document.getElementById("img1").src =  "http://icons.duckduckgo.com/ip1/" + website8 + ".ico";
+  }
+  if (website9 == "") {
+  } else {
+  document.getElementById("img1").src =  "http://icons.duckduckgo.com/ip1/" + website9 + ".ico";
+  }
+  
   urlString =
     "https://linkt.tk?website1=" +
     website1 +
